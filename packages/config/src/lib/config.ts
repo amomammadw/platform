48a0770897e37776d83263e4fddac211fceff5db
MohammadRanjbar <mmohammadrranjbar@gmail.com>
2025-03-06 21:07:02 +0330

    Add config library with initial setup and integration into the website

---
+++ packages/config/src/lib/config.ts
@@ -0,0 +1,8 @@
+import { IConfig } from '../types/config.type';
+
+export const config: IConfig = {
+  name: 'platform',
+  title: 'Platform',
+  description: 'another cool app',
+  author: 'Mohammad Ranjbar',
+};
---
+++ packages/config/src/types/config.type.ts
@@ -0,0 +1,6 @@
+export interface IConfig {
+  name: string;
+  title: string;
+  author?: string;
+  description: string;
+}
