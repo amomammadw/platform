a478d8ea8ec9b08145aa7876da955adbca627cfd
MohammadRanjbar <mmohammadrranjbar@gmail.com>
2025-03-14 12:00:07 +0330

    Add unplugin-icons support and update MainHeader with new icons

--- apps/website/next.config.js
+++ apps/website/next.config.js
@@ -7,6 +7,16 @@ const { composePlugins, withNx } = require('@nx/next');
  * @type {import('@nx/next/plugins/with-nx').WithNxOptions}
  **/
 const nextConfig = {
+  webpack(config) {
+    config.plugins.push(
+      require('unplugin-icons/webpack').default({
+        compiler: 'jsx',
+        jsx: 'react',
+      })
+    );
+
+    return config;
+  },
   nx: {
     // Set this to true if you would like to use SVGR
     // See: https://github.com/gregberge/svgr
--- apps/website/src/components/layout/header/MainHeader.tsx
+++ apps/website/src/components/layout/header/MainHeader.tsx
@@ -1,6 +1,8 @@
 import { HeaderAuthButton } from './HeaderAuthButton';
 import { useGetServerDevice } from '@platform/utils';
 import { HeaderItems } from './HeaderItems';
+import IconAccessible from '~icons/tabler/accessible';
+import IconX from '~icons/tabler/x';
 
 export const MainHeader = async () => {
   const { type } = await useGetServerDevice();
@@ -8,6 +10,8 @@ export const MainHeader = async () => {
   return (
     <header className="sticky bg-white top-0 py-5 shadow-md">
       <nav className="container">
+        <IconAccessible />
+        <IconX />
         <ul className="flex w-full gap-10">
           {type !== 'mobile' && (
             <>
