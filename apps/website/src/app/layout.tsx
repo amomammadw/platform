f51a14a5cd789fc52c8cc3b2d6e2edfcdf5db45a
MohammadRanjbar <mmohammadrranjbar@gmail.com>
2025-03-14 19:04:46 +0330

    Remove favicon.ico and update TypeScript configuration to include favicon path; modify layout to use new favicon source

--- apps/website/src/app/layout.tsx
+++ apps/website/src/app/layout.tsx
@@ -1,4 +1,6 @@
 import { config } from '@platform/config';
+// @ts-expect-error
+import favicon from '@platform/config/favicon';
 import { Dialog } from '@platform/ui';
 import './global.css';
 import { Metadata } from 'next';
@@ -8,6 +10,14 @@ export const metadata: Metadata = {
   title: config.title,
   description: config.description,
   authors: [{ name: config.author?.name, url: config.author?.website }],
+  icons: [
+    {
+      rel: 'icon',
+      type: 'image/x-icon',
+      href: favicon.src,
+      url: favicon.src,
+    },
+  ],
 };
 
 export default function RootLayout({
