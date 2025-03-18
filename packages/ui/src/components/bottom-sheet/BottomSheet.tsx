8881974e4c73325107582d96a2ee1d07dbba4026
MohammadRanjbar <mmohammadrranjbar@gmail.com>
2025-03-18 21:31:52 +0330

    fix: change bottom sheet content

--- packages/ui/src/components/bottom-sheet/BottomSheet.tsx
+++ packages/ui/src/components/bottom-sheet/BottomSheet.tsx
@@ -1,16 +1,34 @@
 'use client';
-import { Drawer } from 'vaul';
+import { ReactNode } from 'react';
+import { Drawer, type DialogProps } from 'vaul';
+import { cn } from '../../utils/cn';
 
-export default function BottomSheet() {
+type TProps = DialogProps & {
+  trigger?: ReactNode;
+  content?: ReactNode;
+  contentClass?: string;
+};
+
+export const BottomSheet = ({
+  trigger,
+  content,
+  contentClass,
+  ...props
+}: TProps) => {
   return (
-    <Drawer.Root>
-      <Drawer.Trigger>Open Drawer</Drawer.Trigger>
+    <Drawer.Root {...props}>
+      <div className="sr-only">
+        <Drawer.Title></Drawer.Title>
+        <Drawer.Description></Drawer.Description>
+      </div>
+
+      <Drawer.Trigger>{trigger}</Drawer.Trigger>
       <Drawer.Portal>
         <Drawer.Overlay className="fixed inset-0 bg-black/40" />
         <Drawer.Content className="bg-gray-100 h-fit fixed bottom-0 left-0 right-0 outline-none">
-          <div className="p-4 bg-white">{/* Content */}</div>
+          <div className={cn('bg-white', contentClass)} children={content} />
         </Drawer.Content>
       </Drawer.Portal>
     </Drawer.Root>
   );
-}
+};
