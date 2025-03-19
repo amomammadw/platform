ed575f4a5efb1fe74eba9f4850cbf44654456e70
MohammadRanjbar <mmohammadrranjbar@gmail.com>
2025-03-19 17:59:28 +0330

    refactor: remove unused Tab component and its related items from the homepage

--- apps/website/src/app/page.tsx
+++ apps/website/src/app/page.tsx
@@ -1,20 +1,11 @@
 import { HomepageHero } from '#/components/homepage/HomepageHero';
 import { HomepageShowcase } from '#/components/homepage/HomepageShowcase';
-import { Login } from '@platform/auth';
-import { ITabItem, Tab } from '@platform/ui';
 
 export default function Index() {
-  const items: ITabItem[] = [
-    { title: 'asdas', content: 'asdasd' },
-    { title: 'asdas2', content: 'Content' },
-    { title: 'asdas3', content: 'asdasd3' },
-  ];
-
   return (
     <div>
       <HomepageHero />
       <HomepageShowcase />
-      <Tab className="mx-auto" items={items} />
     </div>
   );
 }
